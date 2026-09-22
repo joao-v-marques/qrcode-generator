30bb716215e40cb5b7247ee2407d2aab2ac15068
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-22 13:51:55 -0300

    feat: ajustado app.py para inicializar em produção também

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
+import os
 from flask import Flask
 from configs import config_all
+from dotenv import load_dotenv
+from flask_cors import CORS
+from waitress import serve
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+    CORS(app)
     config_all(app)
 
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+
+    enviroment = os.getenv("FLASK_ENV", "development")
+
+    if enviroment == "development":
+        app.run(debug=True)
+    else:
+        print("Servidor Waitress inicializado com sucesso...")
+        serve(app, host='0.0.0.0', port=5007)
